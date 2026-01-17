5291c981396adb85bbeafdc74110f37be027bcfc
Lea Verou <lea@verou.me>
2026-01-17 10:57:57 -0500

    Update nudeps.js

--- nudeps.js
+++ nudeps.js
@@ -1,6 +1,7 @@
 export default {
 	overrides: {
 		imports: {
+			// Override is ./node_modules based, nudeps will transform it accordingly
 			vue: './node_modules/vue/dist/vue.esm-browser.js',
 		},
 	},
