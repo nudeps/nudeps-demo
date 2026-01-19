99e1693c60370002c48933c2509308bdaeaf096e
Lea Verou <lea@verou.me>
2026-01-19 14:40:47 -0500

    React demo now works!

--- react/README.md
+++ react/README.md
@@ -1,6 +1,3 @@
 # Nudeps React Demo
 
-> [!IMPORTANT]
-> This demo is currently broken.
-
 This showcases how CJS modules are handled via requiring `react` directly.
--- react/importmap.js
+++ react/importmap.js
@@ -2,10 +2,25 @@
 let map = {
 	"imports": {
 		"react": "./client_modules/react@19.2.3/index.js",
+		"react-dom": "./client_modules/react-dom@19.2.3/index.js",
+		"react-dom/client": "./client_modules/react-dom@19.2.3/client.js",
+		"react-dom/package.json": "./client_modules/react-dom@19.2.3/package.json",
+		"react-dom/profiling": "./client_modules/react-dom@19.2.3/profiling.js",
+		"react-dom/server": "./client_modules/react-dom@19.2.3/server.browser.js",
+		"react-dom/server.browser": "./client_modules/react-dom@19.2.3/server.browser.js",
+		"react-dom/server.bun": "./client_modules/react-dom@19.2.3/server.bun.js",
+		"react-dom/server.edge": "./client_modules/react-dom@19.2.3/server.edge.js",
+		"react-dom/server.node": "./client_modules/react-dom@19.2.3/server.node.js",
+		"react-dom/static": "./client_modules/react-dom@19.2.3/static.browser.js",
+		"react-dom/static.browser": "./client_modules/react-dom@19.2.3/static.browser.js",
+		"react-dom/static.edge": "./client_modules/react-dom@19.2.3/static.edge.js",
+		"react-dom/static.node": "./client_modules/react-dom@19.2.3/static.node.js",
+		"react-dom/test-utils": "./client_modules/react-dom@19.2.3/test-utils.js",
 		"react/compiler-runtime": "./client_modules/react@19.2.3/compiler-runtime.js",
 		"react/jsx-dev-runtime": "./client_modules/react@19.2.3/jsx-dev-runtime.js",
 		"react/jsx-runtime": "./client_modules/react@19.2.3/jsx-runtime.js",
-		"react/package.json": "./client_modules/react@19.2.3/package.json"
+		"react/package.json": "./client_modules/react@19.2.3/package.json",
+		"scheduler": "./client_modules/scheduler@0.27.0/index.js"
 	},
 	"scopes": {}
 };
@@ -29,9 +44,12 @@ let modules = {};
 
 function resolve (specifier, parentURL = cS.src) {
 	for (let s in map.imports) {
-		if (specifier === s || specifier.startsWith(s + "/")) {
-			specifier = specifier.replace(s, map.imports[s]);
-			return new URL(specifier, parentURL).href;
+		if (specifier === s) {
+			return new URL(map.imports[s], parentURL).href;
+		}
+		if (s.endsWith("/") && specifier.startsWith(s)) {
+			let target = map.imports[s] + specifier.slice(s.length);
+			return new URL(target, parentURL).href;
 		}
 	}
 
@@ -41,7 +59,10 @@ function resolve (specifier, parentURL = cS.src) {
 function require (specifier, parentURL) {
 	let url = specifier;
 
-	if (!url.startsWith(".") && !url.startsWith("https:")) {
+	if (url.startsWith(".")) {
+		url = new URL(url, parentURL ?? cS.src).href;
+	}
+	else if (!url.startsWith("https:")) {
 		url = resolve(specifier, parentURL);
 	}
 
