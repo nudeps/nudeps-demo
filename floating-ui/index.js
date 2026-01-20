e6b2dda1582086bad8bb3f606161cad7d5364bea
Dmitry Sharabin <dmitrysharabin@gmail.com>
2026-01-20 15:15:53 +0100

    [floating-ui] Add simple demo

--- floating-ui/index.js
+++ floating-ui/index.js
@@ -1,2 +1,31 @@
-import { autoPlacement } from "@floating-ui/dom";
-import { shift } from "@floating-ui/core";
+import { computePosition, flip, shift } from "@floating-ui/dom";
+
+const button = document.getElementById("button");
+const tooltip = document.getElementById("tooltip");
+
+function update () {
+	computePosition(button, tooltip, {
+		placement: "top",
+		middleware: [flip(), shift()],
+	}).then(({x, y}) => {
+		Object.assign(tooltip.style, { left: `${x}px`, top: `${y}px` });
+	});
+}
+
+function showTooltip () {
+	tooltip.style.display = "block";
+	update();
+}
+
+function hideTooltip () {
+	tooltip.style.display = "";
+}
+
+[
+	["mouseenter", showTooltip],
+	["mouseleave", hideTooltip],
+	["focus", showTooltip],
+	["blur", hideTooltip],
+].forEach(([event, handler]) => {
+	button.addEventListener(event, handler);
+});
