0272d8126c5952117a000fcc04f4b43be5c7d946
Lea Verou <lea@verou.me>
2026-01-22 17:09:55 -0500

    [jquery] Simplify demo
    Move content to JS so that it's more obvious when it breaks

--- jquery/index.js
+++ jquery/index.js
@@ -6,4 +6,5 @@ const jQuery_1 = require("jquery1");
 const $legacy = jQuery_1.noConflict(true);
 const $ = jQuery.noConflict(true);
 
-$("#count").text($legacy("#list > li").size());
+$("ul").html("jQuery works!".split(/\s+/g).map(word => `<li>${word}</li>`).join("\n"));
+$("#count").text($legacy("ul > li").size());
